7bec9da8e868c7cdf384702cc064e9857c2ded8d
stranger2626 <stranger2626@gmail.com>
2016-10-17 16:25:59 +0300

    changes

--- ModerPattern/Rose.js
+++ ModerPattern/Rose.js
@@ -1,11 +1,14 @@
 var extendDeep = require('./extendDeep.js');
 var Plant = require('./Plant.js');
-var Rose=extendDeep(Plant);
+var Rose={};
+Rose = extendDeep(Plant,Rose);
+console.log(Rose.getName());
+Rose.name='some';
 Rose.hasThorns=!!'true';
 Rose.name='BasicRoseName';
 Rose.color='Red';
 Rose.getColor=function(){
 return this.color;
 };
-console.log(Rose.name);
+console.log(Rose.getName());
 module.exports=Rose;
--- ModerPattern/ThornlessRose.js
+++ ModerPattern/ThornlessRose.js
@@ -1,5 +1,6 @@
 var Rose=require('./Rose.js');
 var extendDeep = require('./extendDeep.js');
-var ThornlessRose = extendDeep(Rose);
+var ThornlessRose={};
+ThornlessRose = extendDeep(Rose,ThornlessRose);
 ThornlessRose.hasThorns=!!'';
 console.log(ThornlessRose.hasThorns+' '+ThornlessRose.name+' '+ThornlessRose.getWaterConsumption()+' '+ThornlessRose.getColor());
